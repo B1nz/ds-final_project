fcb4bc1bf3b51c5a5caddadd885868edea4e9ccd
Luthfi Goldiansyah Kusumajadi <Goldiansyah@gmail.com>
2024-07-28 22:06:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -66,8 +66,8 @@ if model is not None:
         }
 
         # Fill missing month and day columns with 0
-        all_months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-        all_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        all_months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+        all_days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
         for m in all_months:
             if f'month_{m}' not in data:
                 data[f'month_{m}'] = 0
